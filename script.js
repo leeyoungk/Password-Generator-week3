b4338ebbfdbc9e322ccc0b3c74ee0cd4c6ae939c
Khanh Truong <khanhtruong_1992@yahoo.com>
2021-05-21 11:31:14 -0700

    set variables

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 // Assignment Code
+var upperchars= "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowerchars = "abcdefghijklmnopqrstuvwxyz";
+var numbers = "0123456789";
+var specialchars = "@#$";
 var generateBtn = document.querySelector("#generate");
+
+// Declear lowercase array 
+var lowercase = [];
+// Declear uppercase array 
+// Declear numbers array 
+// Declear special characters array 
 
 // Write password to the #password input
 function writePassword() {
